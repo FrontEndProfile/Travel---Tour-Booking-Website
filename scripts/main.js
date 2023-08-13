d3d24b6d2f857d560b01a6cd5ccb917ca06d4d72
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-08-13 21:28:29 +0500

    testing live site

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  speed: 1000,
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      origin: "left center",
+      translate: ["-5%", 0, -200],
+      rotate: [0, 100, 0],
+    },
+    next: {
+      origin: "right center",
+      translate: ["5%", 0, -200],
+      rotate: [0, -100, 0],
+    },
+  },
+});
